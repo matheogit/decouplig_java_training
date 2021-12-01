c77af7eb82800f1ca08509fd6011dd3b964e8336
Louerat Matheo <louerat@et.esiea.fr>
2021-12-01 15:07:43 +0100

    Composite Logger

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -2,6 +2,6 @@ package fr.lernejo.logger;
 
 public abstract class LoggerFactory implements Logger {
     public static Logger getLogger(String name){
-        return new ContextualLogger(name, new FileLogger("txt.txt"));
+        return new CompositeLogger(new ContextualLogger(name, new ConsoleLogger()), new ContextualLogger(name, new FileLogger("log.txt")));
     }
 }
